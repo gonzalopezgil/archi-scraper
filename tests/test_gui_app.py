76d8bfb9d47fc89e1e2a5b4cdfd733a26c967672
Gonzalo López Gil <gonzalo.lopez.gil@gmail.com>
2026-03-08 16:21:02 +0100

    fix(ci): guard all PyQt6-dependent classes inside import check

--- tests/test_gui_app.py
+++ tests/test_gui_app.py
@@ -18,33 +18,30 @@ if HAS_PYQT6:
     os.environ.setdefault("QT_QPA_PLATFORM", "offscreen")
     import ArchiScraperApp as module
 
+    class DummyProfile:
+        def setUrlRequestInterceptor(self, interceptor):
+            self.interceptor = interceptor
 
-class DummyProfile:
-    def setUrlRequestInterceptor(self, interceptor):
-        self.interceptor = interceptor
+    class DummyPage:
+        def __init__(self) -> None:
+            self._profile = DummyProfile()
 
+        def profile(self):
+            return self._profile
 
-class DummyPage:
-    def __init__(self) -> None:
-        self._profile = DummyProfile()
+        def runJavaScript(self, *args, **kwargs):
+            return None
 
-    def profile(self):
-        return self._profile
+    class DummyWebEngineView(QWidget):
+        def __init__(self, *args, **kwargs):
+            super().__init__()
+            self._page = DummyPage()
 
-    def runJavaScript(self, *args, **kwargs):
-        return None
+        def setUrl(self, url):
+            self._url = url
 
-
-class DummyWebEngineView(QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-        self._page = DummyPage()
-
-    def setUrl(self, url):
-        self._url = url
-
-    def page(self):
-        return self._page
+        def page(self):
+            return self._page
 
 
 @unittest.skipUnless(HAS_PYQT6, "PyQt6 not installed")
